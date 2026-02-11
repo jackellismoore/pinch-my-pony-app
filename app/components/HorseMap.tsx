9e5d0eefcab1f35bf19a648220a714f394c90bdc
jackellismoore <118400931+jackellismoore@users.noreply.github.com>
2026-02-11 16:41:32 +0000

    Phase 6 - Marker clustering added

--- app/components/HorseMap.tsx
+++ app/components/HorseMap.tsx
@@ -1,20 +1,17 @@
 "use client";
 
-import {
-  GoogleMap,
-  Marker,
-  InfoWindow,
-  useLoadScript,
-} from "@react-google-maps/api";
-import { useState, useMemo } from "react";
-import Link from "next/link";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 type Horse = {
   id: string;
   name: string;
-  location: string;
   lat: number;
   lng: number;
+  image_url: string;
+  distance?: number;
 };
 
 export default function HorseMap({
@@ -24,60 +21,59 @@ export default function HorseMap({
 }: {
   horses: Horse[];
   userLocation: { lat: number; lng: number } | null;
-  highlightedId?: string | null;
+  highlightedId: string | null;
 }) {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey:
-      process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
+  const center = userLocation
+    ? [userLocation.lat, userLocation.lng]
+    : [51.505, -0.09];
+
+  const defaultIcon = new L.Icon({
+    iconUrl:
+      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
   });
 
-  const [selected, setSelected] = useState<string | null>(null);
-
-  const center = useMemo(() => {
-    if (userLocation) return userLocation;
-    return { lat: 51.505, lng: -0.09 };
-  }, [userLocation]);
-
-  if (!isLoaded) return <div>Loading map...</div>;
+  const highlightedIcon = new L.Icon({
+    iconUrl:
+      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  });
 
   return (
-    <GoogleMap
+    <MapContainer
+      center={center as any}
       zoom={7}
-      center={center}
-      mapContainerStyle={{ width: "100%", height: "600px" }}
+      style={{ height: "600px", width: "100%" }}
     >
-      {horses.map((horse) => (
-        <Marker
-          key={horse.id}
-          position={{ lat: horse.lat, lng: horse.lng }}
-          onClick={() => setSelected(horse.id)}
-          icon={
-            highlightedId === horse.id
-              ? "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-              : undefined
-          }
-        />
-      ))}
+      <TileLayer
+        attribution='&copy; OpenStreetMap contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
 
-      {selected && (
-        <InfoWindow
-          position={{
-            lat: horses.find((h) => h.id === selected)!.lat,
-            lng: horses.find((h) => h.id === selected)!.lng,
-          }}
-          onCloseClick={() => setSelected(null)}
-        >
-          <div>
-            <strong>
-              {horses.find((h) => h.id === selected)!.name}
-            </strong>
-            <br />
-            <Link href={`/request?horseId=${selected}`}>
-              View Horse
-            </Link>
-          </div>
-        </InfoWindow>
-      )}
-    </GoogleMap>
+      <MarkerClusterGroup>
+        {horses.map((horse) =>
+          horse.lat && horse.lng ? (
+            <Marker
+              key={horse.id}
+              position={[horse.lat, horse.lng]}
+              icon={
+                highlightedId === horse.id
+                  ? highlightedIcon
+                  : defaultIcon
+              }
+            >
+              <Popup>
+                <strong>{horse.name}</strong>
+                {horse.distance && (
+                  <div>{horse.distance.toFixed(1)} miles away</div>
+                )}
+              </Popup>
+            </Marker>
+          ) : null
+        )}
+      </MarkerClusterGroup>
+    </MapContainer>
   );
 }
